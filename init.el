af7cab55a0dcd755703cb25773241f59b6888dc1
Caio Carrara <eu@caiocarrara.com.br>
2019-08-25 03:19:13 -0300

    Initial config for Python / Go / Web / JS

--- init.el
+++ init.el
@@ -17,8 +17,28 @@
   (package-install 'use-package))
 
 ;; Packages
-(use-package gruvbox-theme :ensure t :defer 5)
+(use-package gruvbox-theme :ensure t)
+;; Python
+(use-package anaconda-mode :ensure t)
+(add-hook 'python-mode-hook 'anaconda-eldoc-mode)
+;;Go
+(use-package go-mode :ensure t)
+;; HTML / Web
+(use-package web-mode :ensure t)
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq html-indent-level 2)
+;; JS
+(use-package rjsx-mode :ensure t)
+(use-package prettier-js :ensure t)
+(add-to-list 'auto-mode-alist '("\\.js?\\'" . rjsx-mode))
+(setq js-indent-level 2)
+(add-hook 'rjsx-mode-hook 'prettier-js-mode)
 
+;; Theme
 (load-theme 'gruvbox t)
 
 ;; Customization
@@ -32,6 +52,7 @@
 (global-display-line-numbers-mode)
 
 (electric-pair-mode 1)
+(setq-default indent-tabs-mode nil)
 
 (setq custom-file "~/.emacs.d/.emacs.custom.el")
 (load custom-file)
